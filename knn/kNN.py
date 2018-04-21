4d3f82df7827647139bb9fdf01a81310d93441bf
VEHVRBCH <shuchen.zhao@outlook.com>
2018-04-21 17:44:19 +0800

    enter

---
+++ knn/kNN.py
@@ -0,0 +1,27 @@
+from numpy import array, tile
+import operator
+
+
+def createDataSet():
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    # 距离计算
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)  # 将矩阵的每一行向量相加,每一行都是dx^2 + dy^2
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    # 选择距离最小的k个点
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    # 排序
+    sortedClassCount = sorted(
+        classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
